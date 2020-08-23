3802b2c3aa99c26f0d5cf043ad927ba750152034
TatvaShah <tatvashah@users.noreply.github.com>
2020-08-23 19:22:12 -0400

    database config

--- wp-config.php
+++ wp-config.php
@@ -26,16 +26,16 @@ define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settin
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'wordpressecomm' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'ishandb@localhost' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db4free.net' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
